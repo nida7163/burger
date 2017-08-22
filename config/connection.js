765c2ae946e618ef87a046af44ca342f409318ed
Nida Chaudhry <nidachaudhry@lawn-128-61-48-143.lawn.gatech.edu>
2017-08-22 19:12:54 -0400

    uses cleardb

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if(process.env.JAWSDB_URL) {
 } else{
     connection = mysql.createConnection({
         root: 3000,
-        host: 'localhost',
+        host: 'mysql://b7fef98a92bab0:7424b27d@us-cdbr-iron-east-05.cleardb.net/heroku_d7485d75a053874?reconnect=true',
         user: 'root',
         password: '',
         database: 'burgers_db',
