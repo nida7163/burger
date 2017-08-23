6e948506e4604202acb37f750633d089b8d65750
Nida Chaudhry <nidachaudhry@lawn-128-61-48-143.lawn.gatech.edu>
2017-08-22 20:19:08 -0400

    connect changes

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if(process.env.JAWSDB_URL) {
 } else{
     connection = mysql.createConnection({
         root: 3000,
-        host: 'mysql://b7fef98a92bab0:7424b27d@us-cdbr-iron-east-05.cleardb.net/heroku_d7485d75a053874?reconnect=true',
+        host: 'localhost',
         user: 'root',
         password: '',
         database: 'burgers_db',
